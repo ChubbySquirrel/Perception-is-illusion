206f759eb0aba2cd42bd128407a75ad009a83025
Grant Scotto <110574337+ChubbySquirrel@users.noreply.github.com>
2025-08-06 22:54:49 -0400

    Function Continous based bracer movement

--- stone.gd
+++ stone.gd
@@ -1,43 +1,53 @@
 extends Control
 
-var rotate_mode = false
+var rotating = false
 var slide_sense = 1
 var pressed = false
 var initial_mouse_pos: Vector2
 var initial_pos: Vector2
 var initial_rot: float
-var rel_mouse_pos: Vector2
-var rel_rot : float
+
 
 func _process(delta: float) -> void:
-	rotate_mode = Input.is_action_pressed("Rotate")
+	rotating = Input.is_action_pressed("Rotate")
 	var release_rotate = Input.is_action_just_released("Rotate")
 	var start_rotate = Input.is_action_just_pressed("Rotate")
 	
+	
+	
 	if pressed:
-		if release_rotate:
-			initial_mouse_pos = get_global_mouse_position() - position
 		if start_rotate:
 			initial_rot = rotation
-		var current_mouse_pos = get_global_mouse_position()
-		rel_mouse_pos = current_mouse_pos - position
-		if not rotate_mode:
-			position = initial_pos + (current_mouse_pos - (initial_mouse_pos + position))*slide_sense
+			initial_mouse_pos = get_global_mouse_position()
+		if release_rotate:
+			initial_mouse_pos = get_global_mouse_position()
+			initial_pos = position
+			initial_rot = rotation
 			
+		
+		
+		var current_mouse_pos = get_global_mouse_position()
+		var rel_current_mouse_pos = current_mouse_pos - position
+		var rel_initial_mouse_pos = initial_mouse_pos - position
+		
+		if not rotating:
+			position = initial_pos + rel_current_mouse_pos - rel_initial_mouse_pos
 		else:
-			rel_rot = atan2(initial_mouse_pos.x,initial_mouse_pos.y) - atan2(rel_mouse_pos.x,rel_mouse_pos.y)
-			rotation = initial_rot + rel_rot
+			rotation = initial_rot + atan2(rel_initial_mouse_pos.x,rel_initial_mouse_pos.y) - atan2(rel_current_mouse_pos.x,rel_current_mouse_pos.y)
+		
+	
+	
 
 func _on_button_button_down() -> void:
 	pressed = true
-	initial_rot = rotation
-	initial_mouse_pos = get_global_mouse_position() - position
+	#Get current state and save it
+	initial_mouse_pos = get_global_mouse_position()
 	initial_pos = position
+	initial_rot = rotation
 	
 	pass # Replace with function body.
 
 
 func _on_button_button_up() -> void:
 	pressed = false
-	initial_pos = position
 	pass # Replace with function body.
